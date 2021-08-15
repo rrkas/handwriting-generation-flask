e5f71dcb7ddbb1711b67e6ed1683288cc3dfbb8c
Rohnak Agarwal <rrka79wal@gmail.com>
2021-08-15 21:37:23 +0530

    install package from script

--- script.py
+++ script.py
@@ -3,6 +3,9 @@ if __name__ == '__main__':
 
     os.environ['DISPLAY'] = ':0'
 
+    from install_package import install
+    install('flask')
+
     from website import app
     app.run(host='0.0.0.0', port=10002)
 
