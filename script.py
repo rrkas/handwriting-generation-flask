f0ef600d51d694a15109a3c2b8f80869c5834403
Rohnak Agarwal <rrka79wal@gmail.com>
2021-08-15 20:33:10 +0530

    display fix

--- script.py
+++ script.py
@@ -1,4 +1,11 @@
 from website import app
 
 if __name__ == '__main__':
+    import os
+
+    os.environ['DISPLAY'] = ':0'
+
     app.run(host='0.0.0.0', port=10002)
+
+# allow inbound rules
+# sudo ufw allow 10002
